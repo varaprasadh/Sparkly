96579fc24ecc91c27669f7de8adec7220a96300d
varaprasadh2 <varaprasadh.a@imaginnovate.com>
2026-03-04 23:07:28 +0530

    cleanup oauth client ids

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,11 @@ const webpack = require('webpack');
 const { resolve } = require('path');
 const fs = require('fs');
 
-const CLIENT_IDS = {
-    local: '313065344975-ilu2brv8o8hdv00ni6rjk1mhesh1uabl.apps.googleusercontent.com',
-    prod:  '313065344975-k6uda6csaampc8g7i98qpoko9q7fmcr7.apps.googleusercontent.com',
-};
-
 // Read version from manifest.json
 const manifest = JSON.parse(fs.readFileSync('./public/manifest.json', 'utf8'));
 const version = manifest.version || '1.0.0';
 
 // webpack plugin to log the build progress
-
-module.exports = (env = {}) => {
-const clientId = env.prod ? CLIENT_IDS.prod : CLIENT_IDS.local;
 
 const plugins = [
     new ProgressBarPlugin(),
@@ -33,22 +25,14 @@ const plugins = [
     new copyWebpackPlugin({
         patterns:[
             {
-                from: 'public',
-                to: '.',
-                filter: str => !/\.raw.html$/.test(str),
-                transform(content, resourcePath) {
-                    if (!resourcePath.endsWith('manifest.json')) return content;
-                    const json = JSON.parse(content.toString());
-                    json.oauth2.client_id = clientId;
-                    return JSON.stringify(json, null, 4);
-                }
+                from: 'public', to: '.', filter:str=>!/\.raw.html$/.test(str)
             }
         ]
     }),
     new CleanWebpackPlugin()
 ];
 
-return {
+module.exports = {
     mode:'production',
     devtool: 'cheap-module-source-map',
     entry:{
@@ -85,7 +69,6 @@ return {
         ]
     },
     plugins,
-};
 };
 
 
